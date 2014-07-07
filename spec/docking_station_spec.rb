607d61edb1913d1cefbba74ba7bc410e56b8eca2
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-07-07 23:43:22 +0100

    refactored

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,23 +2,22 @@ require 'docking_station'
 
 describe DockingStation do
 
+	let(:old_street) { DockingStation.new }
+	let(:bike)       { double :bike       }
+
 	it 'has no bikes when created' do
-		old_street = DockingStation.new
 		expect(old_street.bikes).to eq []
 	end
 
 	it 'can dock a bike' do
-		old_street = DockingStation.new
-		bike = double :bike
 		old_street.dock bike
 		expect(old_street.bikes).to eq [bike]
 	end
  
 	it 'can release a bike' do
-		bike = double :bike
 		old_street = DockingStation.new(bikes: [bike])
 		old_street.release bike
 		expect(old_street.bikes).to eq []
-	end
+	end	
 
 end
