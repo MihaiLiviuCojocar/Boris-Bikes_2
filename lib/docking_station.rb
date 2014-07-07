5440b45e6f8abc65f7633a7f9af52d96e41031fc
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-07-07 23:35:04 +0100

    docking station cand dock bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,15 @@
 class DockingStation
 
+	def initialize
+		@bikes = []
+	end
+
 	def bikes
-		[]
+		@bikes
+	end
+
+	def dock(bike)
+		bikes << bike
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,17 @@
 require 'docking_station'
 
 describe DockingStation do
+
 	it 'has no bikes when created' do
 		old_street = DockingStation.new
 		expect(old_street.bikes).to eq []
 	end
+
+	it 'can dock a bike' do
+		old_street = DockingStation.new
+		bike = double :bike
+		old_street.dock(bike)
+		expect(old_street.bikes).to eq [bike]
+	end
+
 end
