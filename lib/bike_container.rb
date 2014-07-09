a10d37ee92147476122aba4d4ec889e0f667ffe5
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-07-09 11:24:34 +0100

    exctracted common functionality of a docking station into a module

---
+++ lib/bike_container.rb
@@ -0,0 +1,30 @@
+module BikeContainer
+
+	DEFAULT_CAPACITY = 10
+
+	def bikes
+		@bikes
+	end
+
+	def dock bike
+		raise 'Sorry! No slots available' if full?
+		bikes << bike
+	end
+
+	def release bike
+		bikes.delete bike
+	end
+
+	def full?
+		bikes.count == @capacity
+	end
+
+	def broken_bikes
+		bikes.select { |bike| bike.broken? }
+	end
+
+	def available_bikes
+		bikes - broken_bikes
+	end
+
+end
